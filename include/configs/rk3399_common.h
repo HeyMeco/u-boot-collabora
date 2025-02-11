5ee605672e2f0833d44cfddddf00326d00e2c504
FUKAUMI Naoki <naoki@radxa.com>
2025-02-11 05:33:38 +0000

    rockchip: Add DFU to SPL for RK3399
    Boot the RK3399 from the USB OTG port using rkflashtool[1] or
    rkusbboot[2] and dfu-util.
    
    1a. Download the TPL and SPL using rkflashtool
    
    $ openssl rc4 -provider legacy -K 7c4e0304550509072d2c7b38170d1711 \
    	< mkimage-in-simple-bin.mkimage-rockchip-tpl | rkflashtool l
    $ openssl rc4 -provider legacy -K 7c4e0304550509072d2c7b38170d1711 \
    	< mkimage-in-simple-bin.mkimage-u-boot-spl | rkflashtool L
    
    1b. Download the TPL and SPL using rkusbboot
    
    $ rkusbboot \
    	mkimage-in-simple-bin.mkimage-rockchip-tpl \
    	mkimage-in-simple-bin.mkimage-u-boot-spl
    
    2. Download the U-Boot
    
    $ dfu-util -a 0 -D u-boot.itb
    
    [1] https://github.com/linux-rockchip/rkflashtool
    [2] https://github.com/RadxaNaoki/rkusbboot
    
    Signed-off-by: FUKAUMI Naoki <naoki@radxa.com>

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -13,8 +13,6 @@
 #define CFG_SYS_SDRAM_BASE		0
 #define SDRAM_MAX_SIZE			0xf8000000
 
-#ifndef CONFIG_XPL_BUILD
-
 #ifndef ROCKCHIP_DEVICE_SETTINGS
 #define ROCKCHIP_DEVICE_SETTINGS
 #endif
@@ -31,13 +29,18 @@
 	"ramdisk_addr_r=0x12180000\0"	\
 	"kernel_comp_size=0x8000000\0"
 
+#if defined(CONFIG_SPL_DFU) && defined(CONFIG_XPL_BUILD)
+#define CFG_EXTRA_ENV_SETTINGS				\
+	"dfu_alt_info_ram=u-boot.itb ram "		\
+	__stringify(CONFIG_SPL_LOAD_FIT_ADDRESS) " "	\
+	__stringify(CONFIG_SYS_DFU_MAX_FILE_SIZE) "\0"
+#else
 #define CFG_EXTRA_ENV_SETTINGS		\
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0"	\
 	"partitions=" PARTS_DEFAULT	\
 	ENV_MEM_LAYOUT_SETTINGS		\
 	ROCKCHIP_DEVICE_SETTINGS	\
 	"boot_targets=" BOOT_TARGETS "\0"
-
-#endif /* CONFIG_XPL_BUILD */
+#endif
 
 #endif /* __CONFIG_RK3399_COMMON_H */
